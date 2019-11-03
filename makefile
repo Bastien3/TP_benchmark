dcce725a9c4cdda2cd8bcc077b5d252feaad9cd1
Bastien <Bastien.Simon.Etu@univ-lemans.fr>
2019-11-03 20:53:00 +0100

    -

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
+FLAGS=-pedantic -pedantic-errors -Wextra -Wall -Waggregate-return -Wno-attributes -Wcast-align -Wcast-qual -Wchar-subscripts -Wcomment -Wconversion -Wno-deprecated-declarations -Wdisabled-optimization -Wno-div-by-zero -Wno-endif-labels -Werror -Werror-implicit-function-declaration -Wfatal-errors -Wfloat-equal -Wformat -Wformat=2 -Wno-format-extra-args -Wformat-nonliteral -Wformat-security -Wformat-y2k -Wimplicit -Wimplicit-function-declaration -Wimplicit-int -Wimport -Wno-import -Winit-self -Winline -Wno-int-to-pointer-cast -Winvalid-pch -Wunsafe-loop-optimizations -Wmain -Wmissing-braces -Wmissing-field-initializers -Wmissing-format-attribute -Wmissing-include-dirs -Wmissing-noreturn -Wno-multichar -Wnonnull -Wpacked -Wparentheses -Wpointer-arith -Wno-pointer-to-int-cast -Wreturn-type -Wsequence-point -Wshadow -Wsign-compare -Wstack-protector -Wstrict-aliasing -Wstrict-aliasing=2 -Wswitch -Wswitch-default -Wswitch-enum -Wsystem-headers -Wtrigraphs -Wundef -Wuninitialized -Wunknown-pragmas -Wno-pragmas -Wunreachable-code -Wunused -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value -Wunused-variable -Wvariadic-macros -Wvolatile-register-var -Wwrite-strings
+
 tempsmoy : tempsmoy.c
-	gcc tempsmoy.c -o tempsmoy -Wall -ansi -pedantic-errors
+	gcc tempsmoy.c -o tempsmoy $(FLAGS)
 clean :
-	rm tempsmoy.o
 	rm tempsmoy
 	
--- tempsmoy.c
+++ tempsmoy.c
@@ -6,14 +6,14 @@
 
 int main (int argc, char *argv[]) {
 
-	int k, n, i, j;
+	int k, n, i, j, mediane;
 	int cr;
 	char commande[50];
 	struct timeval temps_debut, temps_fin;
 	suseconds_t temps_ecoule;
 	int somme_temps = 0;
-	float *tab, moyenne;
-	int tube[2];
+	float *tab, moyenne, en_cours, m;
+	int tube[2], tube2[2];
 	pid_t pid_fils;
 
 	if (argc != 4) {
@@ -22,59 +22,96 @@ int main (int argc, char *argv[]) {
 	}
 	k = atoi(argv[1]);
 	n = atoi(argv[3]);
+	mediane = n / 2;
 	sprintf(commande, "%s", argv[2]);
-	printf("%d %s %d\n", n, commande, k);
-	tab = malloc(sizeof(float) * n);
+	tab = malloc(sizeof(float) * (unsigned int)n);
 
 	for (i = 0 ; i < n ; i++) {
+
 		/* Lancement des n processus fils */
 		pipe(tube);
 		switch(fork()) {
+
 			case -1 : 
-				perror("Problème dans le processus fils");
+				perror("Problème dans le processus fils.\n");
 				exit(-1);
+
 			case 0 : 
 				for (j = 0 ; j < k ; j++) {
 					/* Lancement des k exécutions de la commande */
+					pipe(tube2);
 					switch (fork()) {
 						case -1 : 
 							perror("Problème dans le processus fils : ");
 							exit(-1);
 						case 0 : 
+							/* Redirection de la partie lecture du tube vers l'entrée standard */
+							/* Objectif : éviter l'affichage de la commande à l'écran */
+							close(tube2[1]);
+							close(0);
+							dup(tube2[0]);
+							close(tube2[0]);
+
 							execlp(commande, argv[0], NULL);
 							perror("Problème dans l'exécution de la commande : ");
 							exit(-1);
 						default : 
+							/* Redirection de la partie écriture du tube vers la sortie standard */
+							close(tube2[0]);
+							close(1);
+							dup(tube2[1]);
+							close(tube2[1]);
+
 							/* Calcul du temps de la commande */
 							gettimeofday(&temps_debut, NULL);
 							wait(&cr);
 							gettimeofday(&temps_fin, NULL);
 							temps_ecoule = temps_fin.tv_usec - temps_debut.tv_usec;
 							somme_temps += (int)temps_ecoule;
-							
-							/*printf("Temps écoulé : %d microsecondes\n", (int)temps_ecoule);*/
+
+							/* Cas où la commande s'est terminée anormalement */
+							if (cr << 8 == 0 && cr >> 8 != 0) /* Code de retour de la commande != 0 */
+								printf("Échec de la commande : retour %d.\n", cr >> 8);
+							else if (cr << 8 != 0) /* Commande interrompue par un signal */
+								printf("Commande interrompue par le signal %d.\n", cr << 8);
 					}
 				}
-				moyenne = (float)somme_temps / k;
-				printf("%f\n", moyenne);
-				printf("Tube du processus %d : lecture %d écriture %d\n", getpid(), tube[0], tube[1]);
+
+				moyenne = (float)somme_temps / (float)k;
+
+				/* Écriture de la moyenne calculée par le ième fils dans le tube */
 				close(tube[0]);
 				write(tube[1], &moyenne, sizeof(float));
 				close(tube[1]);
 				exit(0);
+
 			default : 
 				while((pid_fils = wait(&cr)) != -1) {
-					printf("pid fils : %d\n", (int)pid_fils);
-					printf("Tube du processus %d : lecture %d écriture %d\n", getpid(), tube[0], tube[1]);
+
+					/* Récupération de la moyenne */
 					close(tube[1]);
 					read(tube[0], &moyenne, sizeof(float));
-					printf("%f\n", moyenne);
+					tab[i] = moyenne;
 				}
 		}
 	}
-	/*for (j = 0 ; j < n ; j++)
-		printf("%f ", tab[j]);*/
+	
+	/* Tri du tableau par insertion */
+	for (i = 1 ; i < n ; i++) {
+		en_cours = tab[i];
+		for (j = i ; j > 0 && tab[j - 1] > en_cours ; j--)
+			tab[j] = tab[j - 1];
+		tab[j] = en_cours;
+	}
+
+	/* Calcul du temps moyen d'exécution par la médiane */
+	if (n % 2 == 0)
+		m = (tab[mediane - 1] + tab[mediane]) / 2;
+	else
+		m = tab[mediane];
+
 	printf("\n");
+	printf("Temps moyen : %.3f millisecondes.\n", m / 1000);
 	free(tab);
 	return 0;
 }
